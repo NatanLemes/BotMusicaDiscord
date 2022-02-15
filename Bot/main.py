0e970b888800047143fb148b8aa1ed6d24d9f79f
NatanLemes <57756462+NatanLemes@users.noreply.github.com>
2022-02-15 13:40:11 -0300

    Bot Musica Discord

---
+++ Bot/main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+testing = False
+
+client = commands.Bot(command_prefix = "!", case_insensitive = True, intents=intents)
+
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('Entramos como {0.user}'.format(client))
+
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="músicas"))
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run('SEU CODIGO DO BOT AQUI')
+# pip3 install -U discord.py[voice]
